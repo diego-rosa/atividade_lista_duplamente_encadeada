a80042c0a9e08c89978f0c25a259d6eeb141a45f
diego-rosa <diego.jose.rosa.da.silva@gmail.com>
2022-12-06 23:29:55 -0300

    adicionando novos arquivos

---
+++ main.py
@@ -0,0 +1,10 @@
+from Lista import Lista
+
+l1 = Lista()
+
+l1.imprimir()
+l1.adicionarNoInicio("Diego")
+l1.adicionarNoInicio("Amanda")
+l1.adicionarNoFim("Leandro")
+l1.removerDoInicio()
+l1.removerDoFim()
